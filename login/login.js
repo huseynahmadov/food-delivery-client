0855a62c370e4a3e11e4f4a1839d4210038714a5
huseynahmadov <t.huseyn96@mail.ru>
2022-03-19 22:39:28 +0400

    responsive function added

--- login/login.js
+++ login/login.js
@@ -2,9 +2,13 @@
 $('#signup-btn').on('click', function() {
     $('.login-container').css("display","none");
     $('.signup-container').css("display","block");
+
+    $('.signup').css("margin-top","400px");
 });
 
 $('#login-btn').on('click', function() {
     $('.login-container').css("display","block");
     $('.signup-container').css("display","none");
+
+    $('.signup').css("margin-top","100px");
 });
