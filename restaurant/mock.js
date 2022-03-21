1bdac20c056adcc5d9736b327147240570e8ebab
huseynahmadov <t.huseyn96@mail.ru>
2022-03-22 01:01:40 +0400

    updated

---
+++ restaurant/mock.js
@@ -0,0 +1,77 @@
+
+export const restaurants = [
+  {
+    id: "r1",
+    name: "Coffee Mania",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Fast Food",
+    img: "/public/images/restaurants/coffee-cup.gif",
+    location: "Jafar Jabbarli Street 24A"
+  },
+  {
+    id: "r2",
+    name: "Burger King",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Fast Food",
+    img: "/public/images/restaurants/burger.png",
+    location: "Nizami Street"
+  },
+  {
+    id: "r3",
+    name: "Mc Donalds",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Fast Food",
+    location: "Neftchilar Avenue "
+  },
+  {
+    id: "r4",
+    name: "Maharaja Restaurant",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Fast Food",
+    location: "Neftchilar Avenue "
+  },
+  {
+    id: "r5",
+    name: "Dera Restaurant",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Pizza",
+    location: "Nizami Street"
+  },
+  {
+    id: "r6",
+    name: "Hong Kong Chinese Restaurant",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Pizza",
+    location: "Puskin Street"
+  },
+  {
+    id: "r7",
+    name: "Papa Johns",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Pizza",
+    location: "Fetch Street flex"
+  },
+  {
+    id: "r8",
+    name: "Dominos",
+    dess: "chinese, sea-food, thai, lebanese, caribbean",
+    price: 5,
+    delivery: 9,
+    type: "Pizza",
+    location: "Fetch Street flex"
+  },
+];
