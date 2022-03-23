c2bc292d828f43adaa7e7f67378d6203a266cedb
thehasanovv <hasanov.e95@gmail.com>
2022-03-23 20:03:19 +0400

    add to basket

--- restaurant/restaurant.js
+++ restaurant/restaurant.js
@@ -1,8 +1,57 @@
 $(document).ready(function () {
+  const initialState = { items: [], totalPrice: 0, totalQuantity: 0 };
   const data = JSON.parse(localStorage.getItem("res"));
+  const basket = JSON.parse(localStorage.getItem("basket"));
+  let newItems = initialState.items.slice();
+
+  const updateBasket = () => {
+    if (basket === null) {
+      return;
+    } else {
+      console.log(basket.items);
+      basket.items.map((item) =>
+        $(".pr-container").append(
+          `  
+          <div class="product-item">
+          <div class="product-item-delete">
+            <img src="/public/images/deleteitem.svg" alt="" />
+          </div>
+          <div class="product-item-count">
+            <span>+</span>
+            <p>1</p>
+            <span>-</span>
+          </div>
+          <div class="product-item-desc">
+            <img src="${item.img}" alt="" />
+            <div>
+              <p>${item.name}</p>
+              <p>$${item.price}</p>
+            </div>
+          </div>
+        </div>`
+        )
+      );
+    }
+  };
+  updateBasket();
 
   const addProductHandler = (item) => {
-    console.log(item);
+    const basket = JSON.parse(localStorage.getItem("basket"));
+
+    if (basket === null) {
+      newItems.push(item);
+      initialState.items = newItems.slice();
+      localStorage.setItem("basket", JSON.stringify(initialState));
+      updateBasket();
+    } else {
+      const newBasket = basket.items;
+      newItems.push(item);
+      initialState.items = newBasket.concat(newItems);
+      localStorage.setItem("basket", JSON.stringify(initialState));
+      newItems = [];
+      updateBasket();
+    }
+    // const existingItem = basket.items.find((items) => console.log(items));
   };
 
   data.at(0).products.map((item) =>
@@ -28,6 +77,6 @@ $(document).ready(function () {
   $(`.add-product`).on("click", function () {
     var dataId = $(this).attr("data-id");
     const item = data.at(0).products.filter((item) => item.id === dataId);
-    addProductHandler(item);
+    addProductHandler(item.at(0));
   });
 });
