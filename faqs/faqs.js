88bf87739fe7f9279d6c418d024bc7ac6f058c46
thehasanovv <hasanov.e95@gmail.com>
2022-03-27 16:46:36 +0400

    update

--- faqs/faqs.js
+++ faqs/faqs.js
@@ -1,18 +1,28 @@
 import { questions } from "./questions.js";
 
 $(document).ready(function () {
-  $(".plus").on("click", function () {
+  $(".acc").on("click", function (e) {
     var dataId = $(this).attr("data-id");
-    $(`.plus[data-id='${dataId}']`).css({ display: "none" });
-    $(`.minus[data-id='${dataId}']`).css({ display: "block" });
-    $(`.acc-body[data-id='${dataId}']`).addClass("active");
+    // $(`.plus[data-id='${dataId}']`).css({ display: "none" });
+    // $(`.minus[data-id='${dataId}']`).css({ display: "block" });
+    // $(`.acc-body[data-id='${dataId}']`).addClass("active");
+    if ($(e.target).hasClass("fa-plus")) {
+      $(this).find(".plus").css({ display: "none" });
+      $(this).find(".minus").css({ display: "block" });
+      $(this).find(".acc-body").addClass("active");
+    }
   });
 
-  $(".minus").on("click", function () {
+  $(".acc").on("click", function (e) {
     var dataId = $(this).attr("data-id");
-    $(`.plus[data-id='${dataId}']`).css({ display: "block" });
-    $(`.minus[data-id='${dataId}']`).css({ display: "none" });
-    $(`.acc-body[data-id='${dataId}']`).removeClass("active");
+    // $(`.plus[data-id='${dataId}']`).css({ display: "block" });
+    // $(`.minus[data-id='${dataId}']`).css({ display: "none" });
+    // $(`.acc-body[data-id='${dataId}']`).removeClass("active");
+    if ($(e.target).hasClass("fa-minus")) {
+      $(this).find(".plus").css({ display: "block" });
+      $(this).find(".minus").css({ display: "none" });
+      $(this).find(".acc-body").removeClass("active");
+    }
   });
 });
 
